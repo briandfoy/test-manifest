c8dcb9824c48be19702e4fe6826cbb5f09a0f7b9
brian d foy <brian.d.foy@gmail.com>
2005-03-19 18:35:04 +0000

    * Added POD coverage tests
    * Instead of fooling ExtUtils::*, redefine things through MY::*. I knew
    I should have done things that way, but Schwern reminded me.
    
    
    git-svn-id: http://brian-d-foy.svn.sourceforge.net/svnroot/brian-d-foy/trunk@1633 d9f60a02-fc28-0410-9752-bc0d6c4f57a6

--- lib/Manifest.pm
+++ lib/Manifest.pm
@@ -18,20 +18,14 @@ my $Manifest = catfile( "t", "test_manifest" );
 
 require 5.006;
 
-use ExtUtils::MM_Any;
-
-{
-local $^W = 0;
-
-* ExtUtils::MM_Any::test_via_harness = sub
+*MY::test_via_harness = sub
 	{
 	my($self, $perl, $tests) = @_;
 
 	return qq|\t$perl "-MTest::Manifest" | .
 		   qq|"-e" "run_t_manifest(\$(TEST_VERBOSE), '\$(INST_LIB)', | .
 		   qq|'\$(INST_ARCHLIB)')"\n|;
-	}
-}
+	};
 
 =head1 NAME
 
@@ -47,27 +41,26 @@ Test::Manifest - interact with a t/test_manifest file
 	
 =head1 DESCRIPTION
 
-Test::Harness assumes that you want to run all of the .t files
-in the t/ directory in ascii-betical order during C<make test>
-unless you say otherwise.  This leads to some interesting
-naming schemes for test files to get them in the desired order.
-This interesting names ossify when they get into source control,
-and get even more interesting as more tests show up.
+Test::Harness assumes that you want to run all of the .t files in the
+t/ directory in ascii-betical order during C<make test> unless you say
+otherwise.  This leads to some interesting naming schemes for test
+files to get them in the desired order. This interesting names ossify
+when they get into source control, and get even more interesting as
+more tests show up.
 
 Test::Manifest overrides the default behaviour by replacing the
 test_via_harness target in the Makefile.  Instead of running at the
-t/*.t files in ascii-betical order, it looks in the t/test_manifest file
-to find out which tests you want to run and the order in which you want
-to run them.  It constructs the right value for MakeMaker to do the
-right thing.
+t/*.t files in ascii-betical order, it looks in the t/test_manifest
+file to find out which tests you want to run and the order in which
+you want to run them.  It constructs the right value for MakeMaker to
+do the right thing.
 
 In t/test_manifest, simply list the tests that you want to run.  Their
 order in the file is the order in which they run.  You can comment
 lines with a #, just like in Perl, and Test::Manifest will strip
 leading and trailing whitespace from each line.  It also checks that
-the specified file is actually in the t/ directory.  If the file
-does not exist, it does not put its name in the list of test
-files to run.
+the specified file is actually in the t/ directory.  If the file does
+not exist, it does not put its name in the list of test files to run.
 
 =head2 Functions
 
@@ -78,25 +71,7 @@ files to run.
 Run all of the files in t/test_manifest through Test::Harness:runtests
 in the order they appear in the file.
 
-If you want to use this, in Makefile.PL you need to override some
-MakeMaker magic (after you load ExtUtils::MakeMaker).  I recommend
-putting something like this directly in Makefile.PL so it does not
-depend on anything else.  A Makefile.PL with dependencies is a big
-headache.
-
 	eval "use Test::Manifest";
-
-	unless( $@ )
-		{
-		* ExtUtils::MM_Any::test_via_harness = sub
-			{
-			my($self, $perl, $tests) = @_;
-
-			return qq|\t$perl "-MTest::Manifest" | .
-				   qq|"-e" "run_t_manifest(\$(TEST_VERBOSE), '\$(INST_LIB)', | .
-				   qq|'\$(INST_ARCHLIB)')"\n|;
-			}
-		}
 
 =cut
 
